781b1f624d84557af8bbf5e0c5db4b4a887f11aa
Gogul-D <gokul.d.becse@gmail.com>
2024-02-18 15:18:53 +0530

    create unary operations

---
+++ Unary operator/src/unary operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include <conio.h>
+ int main() {
+	 int a , b;
+	 a=1;
+	 b=2;
+	 printf("%d \n",a);
+	 a++;
+	 printf("%d \n",a); /*after increment operator used*/
+	 printf("%d \n",b);
+	 	 b--;
+	 	 printf("%d",b); /*after decrement operator used*/
+	 return 0;
+}
