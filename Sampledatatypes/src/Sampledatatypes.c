c6833f6668ec4ea3fda9c09d67ed62fbe1acdac6
Gogul-D <gokul.d.becse@gmail.com>
2024-02-18 11:42:58 +0530

    create datatypes

---
+++ Sampledatatypes/src/Sampledatatypes.c
@@ -0,0 +1,25 @@
+/*
+ ============================================================================
+ Name        : Sampledatatypes.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a =10;
+	float b=1.0000;
+	char c ='g' ;
+	double  d = 1245521.000000000; /* double float value*/
+	printf("\n the integer value:%d",a);
+	printf("\n the integer value:%f",b);
+	printf("\n the integer value:%c",c);
+	printf("\n the integer value:%f",d);
+
+	return EXIT_SUCCESS;
+}
