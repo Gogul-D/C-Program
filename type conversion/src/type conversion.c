9a51e928f6efc67666e76e52bd488b7f308fa803
Gogul-D <gokul.d.becse@gmail.com>
2024-02-18 21:29:50 +0530

    Type conversion

---
+++ type conversion/src/type conversion.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : type.c
+ Author      : gokul
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int x =200;
+	int y =300;
+	float val = (float) x/y;
+	printf("%.2f",val);
+
+	return EXIT_SUCCESS;
+}
