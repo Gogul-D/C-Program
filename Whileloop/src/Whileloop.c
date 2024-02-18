4e55f58e26ba928ba9e3f121ae50153960111813
Gogul-D <gokul.d.becse@gmail.com>
2024-02-18 15:33:38 +0530

    while loop

---
+++ Whileloop/src/Whileloop.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : Whileloop.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int i=0 ;
+
+	while(i<=10){
+
+		printf("\n%d",i);
+		i++;
+	}
+
+	return EXIT_SUCCESS;
+}
