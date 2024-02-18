c5462d58ad3cbeaca97b8abc3ffbd4cc55a989eb
Gogul-D <gokul.d.becse@gmail.com>
2024-02-18 12:10:07 +0530

    intial stage common math oprations

---
+++ arthimeticoprations/src/arthimeticoprations.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	 int num1 =10 ,num2 =20;
+	 printf(" addition of num1 and num2:%d ",num1+num2);
+	 printf("\n subtraction of num1 and num2:%d ",num1-num2);
+	 printf("\n multiplication of num1 and num2:%d ",num1*num2);
+	 printf("\n division of num1 and num2:%d ",num1/num2);/* given output is quotient */
+	 printf("\n modulo of num1 and num2:%d ",num1%num2);/* given output is remainder */
+	return EXIT_SUCCESS;
+}
