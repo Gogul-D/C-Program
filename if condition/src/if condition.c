5851a22da2851d49bb2cb3ea0635a852f3fd9d6d
Gogul-D <gokul.d.becse@gmail.com>
2024-02-18 15:21:47 +0530

    create conditional stsements

---
+++ if condition/src/if condition.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : if.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main(void) {
+	if (5>2) {
+
+		printf("true!");/*when condition is true output is printer otherwise not output printer*/
+	}
+
+	return EXIT_SUCCESS;
+}
